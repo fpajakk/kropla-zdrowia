30b6425563ab2c464bf1d2a5c610bea0198ce9f0
filippajak <filip.pajak@nask.pl>
2025-01-06 01:23:56 +0100

    g

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-smoothscroll`,
+    'gatsby-plugin-image',
     {
       resolve: "gatsby-plugin-robots-txt",
       options: {
